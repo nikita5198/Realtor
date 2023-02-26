4a4cc1ada42d3f072458aedeaf280eea7f675a51
nikita5198 <nikitap5198@gmail.com>
2023-02-26 17:06:31 +0530

    Pages and routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
 import './App.css';
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
 function App() {
   return (
     <>
-    <h1 className={"text-blue-700"}> Hello</h1>
+     <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/forgot-password' element={<ForgotPassword/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+      </Routes>
+     </Router>
     </>
   );
 }
